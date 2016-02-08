5fb8cdf8a277e8b26cc0610da03dbe069bed117a
wendyperalta <wendy.prlt511@gmail.com>
2016-02-07 23:29:32 -0800

    calendar populates from data.

--- app/calendar/controller.js
+++ app/calendar/controller.js
@@ -1,18 +1,58 @@
 myApp.controller('calendarController', ['$scope', 'moment', function($scope, moment) {
-  $scope.todayDate = moment().format('LL');
+  var dateObj = new Date();
+  var month = dateObj.getUTCMonth() + 1; //months from 1-12
+  var year = dateObj.getUTCFullYear();
+
+  $scope.month = month;
+  $scope.year = year;
+
+  $scope.users = {
+    "name" : {
+        "first" : "Wendy",
+        "last" : "Peralta"
+    },
+    "roles" : [
+        "admin",
+        "volunteer"
+    ],
+    "shifts" : [
+        {
+            "name" : "Morning",
+            "time" : {
+                "start" : new Date("2016-02-06T18:00:00.000Z"),
+                "end" : new Date("2016-02-06T19:00:00.000Z")
+            }
+        },
+        {
+            "name" : "Afternoon",
+            "time" : {
+                "start" : new Date("2016-02-13T21:00:00.000Z"),
+                "end" : new Date("2016-02-13T22:00:00.000Z")
+            }
+        }
+    ]
+  };
 
   //calendar variables
   this.calendarView = 'month';
   this.calendarDate = new Date();
-  this.events = [
-    {
-        title: 'Morning shift',
-        type: 'info',
-        startsAt: moment().add(7, 'days').toDate(),
-        endsAt: moment().add(7, 'days').add(4, 'hours').toDate(),
-        recursOn: 'month',
-        draggable: false,
-        resizable: true
-      }
-  ];
+  this.events = [];
+  for(var i = 0; i < $scope.users.shifts.length; i++){
+    var eventObject = {
+      title: $scope.users.shifts[i].name + ' shift',
+      type: 'info',
+      startsAt: $scope.users.shifts[i].time.start,
+      endsAt: $scope.users.shifts[i].time.end,
+      recursOn: 'month',
+      draggable: false,
+      resizable: true
+    };
+    this.events.push(eventObject);
+  }
+}]).filter('monthName', [function() {
+    return function (monthNumber) { //1 = January
+        var monthNames = [ 'January', 'February', 'March', 'April', 'May', 'June',
+            'July', 'August', 'September', 'October', 'November', 'December' ];
+        return monthNames[monthNumber - 1];
+    };
 }]);

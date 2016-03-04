d43ac469e02cd4c6727a23d9391696fd6bf6262c
emilguareno <emil@arcticloop.com>
2016-03-04 00:59:38 -0800

    Adding date and time pickers and finalizing shifts form to submit to api.

--- app/index.js
+++ app/index.js
@@ -1,21 +1,28 @@
-var myApp = angular.module('myApp', ['ui.router', 'mwl.calendar', 'ui.bootstrap', 'ngTagsInput']).config([
+var myApp = angular.module('myApp', [
+  'ui.router',
+  'mwl.calendar',
+  'ui.bootstrap',
+  'ui.bootstrap.datetimepicker',
+  'ngTagsInput'
+]).config([
   '$stateProvider',
   '$urlRouterProvider',
   function($stateProvider, $urlRouterProvider) {
-  //
-  // For any unmatched url, redirect to /
-  $urlRouterProvider.otherwise("/");
-  //
-  // Now set up the states
-  $stateProvider
-    .state('calendar', {
-      url: "/",
-      templateUrl: "calendar/index.html",
-      controller: 'calendarController',
-      controllerAs: 'calendar'
-    }).state('shifts', {
-      url: "/shifts",
-      templateUrl: "shifts/index.html",
-      controller: 'shiftsController'
-    });
-}]);
+    //
+    // For any unmatched url, redirect to /
+    $urlRouterProvider.otherwise("/");
+    //
+    // Now set up the states
+    $stateProvider
+      .state('calendar', {
+        url: "/",
+        templateUrl: "calendar/index.html",
+        controller: 'calendarController',
+        controllerAs: 'calendar'
+      }).state('shifts', {
+        url: "/shifts",
+        templateUrl: "shifts/index.html",
+        controller: 'shiftsController'
+      });
+  }
+]);

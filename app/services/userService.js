32f84103d1205273e306fdbc8fb715d35d63af1e
emilguareno <emil@arcticloop.com>
2016-03-06 21:51:32 -0800

    Adding popup to calendar

--- app/services/userService.js
+++ app/services/userService.js
@@ -1,30 +1,16 @@
 myApp.service('userService', ['$http', '$q', function($http, $q) {
   this.getAllUsers = function() {
-    var deferred = $q.defer();
-    if (this.users) {
-      deferred.resolve(this.users);
-    } else {
-      $http.get('/api/users').then(function(users) {
-        this.users = users;
-        deferred.resolve(users.data);
-      }.bind(this));
-    }
-    return deferred.promise;
+    $http.get('/api/users').then(function(users) {
+      deferred.resolve(users.data);
+    }.bind(this));
   };
   this.getAllUserNames = function() {
     return this.getAllUsers().then(function(users) {
       var userNames = users.map(function(user) {
         var userInfo = {
-          id: user._id
+          id: user._id,
+          name: user[user.method].name
         };
-        if (user.facebook) {
-          userInfo.name = user.facebook.name;
-        }
-        else if (user.twitter) {
-          userInfo.name = user.twitter.displayName;
-        } else if (user.local) {
-          userInfo.name =  user.local.name;
-        }
         return userInfo;
       });
       return userNames;

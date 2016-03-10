dec83027b8e52362260469716fe176211cad0cea
emilguareno <emil@arcticloop.com>
2016-03-10 08:48:34 -0800

    Fixing user service and adding notifications module

--- app/services/userService.js
+++ app/services/userService.js
@@ -1,8 +1,10 @@
 myApp.service('userService', ['$http', '$q', function($http, $q) {
   this.getAllUsers = function() {
+    var deferred = $q.defer();
     $http.get('/api/users').then(function(users) {
       deferred.resolve(users.data);
     }.bind(this));
+    return deferred.promise;
   };
   this.getAllUserNames = function() {
     return this.getAllUsers().then(function(users) {

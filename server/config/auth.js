6829140aba8cc02485d07b56ebddd9de29923310
emilguareno <emil@arcticloop.com>
2016-03-19 19:01:05 -0700

    Adding navigation to the dashboard

--- server/config/auth.js
+++ server/config/auth.js
@@ -2,14 +2,14 @@
 module.exports = {
 
     'facebookAuth' : {
-        'clientID'      : '***REMOVED***', // your App ID
-        'clientSecret'  : '***REMOVED***', // your App Secret
+        'clientID'      : '507418682761621', // your App ID
+        'clientSecret'  : '5a5a160e48c1c219603e394ec7ee87cd', // your App Secret
         'callbackURL'   : 'http://localhost:3000/auth/facebook/callback'
     },
 
     'twitterAuth' : {
-        'consumerKey'       : '***REMOVED***',
-        'consumerSecret'    : '***REMOVED***',
+        'consumerKey'       : 'AUXjN3aiHu2FheFeKvaHJJYJU',
+        'consumerSecret'    : 'Ug4b5NTw8tpSvsVAzABKxWs0I8V7rpUvuAdyyH6hEfIgO8RkT2',
         'callbackURL'       : 'http://localhost:3000/auth/twitter/callback'
     },
 
